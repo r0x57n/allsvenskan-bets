523e98aeebde9493765f5418c81655af633b8aa6
r0x57n <contact@0x57.me>
2022-08-22 14:30:58 +0200

    how to suggest features

--- src/infoCommand.go
+++ src/infoCommand.go
@@ -6,7 +6,8 @@ import (
 
 func (b *botHolder) infoCommand(i *dg.InteractionCreate) {
     str := "Jag är en bot gjord i Go med hjälp av [discordgo](https://github.com/bwmarrin/discordgo) paketet. Min källkod finns på [Github](https://github.com/r0x57n/allsvenskanBets). "
-    str += "Just nu kör jag version " + VERSION + "."
+    str += "Just nu kör jag version " + VERSION + ".\n\n"
+    str += "Förbättringsförslag mottages i #bets kanalen."
 
     fields := []*dg.MessageEmbedField {}
     addEmbeddedInteractionResponse(b.session, i, NewMsg, fields, "Hej!", str)
